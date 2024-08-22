0655cdb14865ecc37e230ed354886f3f93e7d0c5
Lucas <lucas.silva@agencian1.com.br>
2024-08-21 23:19:15 -0300

    Update methods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -13,6 +13,22 @@ export class UserEntity extends Entity<UserProps> {
         this.props.createdAt = this.props.createdAt ?? new Date();
     }
 
+    update(value: string):void {
+        this.name = value;
+    }
+
+    updatePassword(value: string):void {
+        this.password = value;
+    }
+
+    private set name(value: string){
+        this.name = value;
+    }
+
+    private set password(value: string){
+        this.password = value;
+    }
+
     get name() {
         return this.props.name;
     }
