24fa750c12c00b9af078d45fef04aee27f9016d1
Lucas <lucas.silva@agencian1.com.br>
2024-08-24 00:59:58 -0300

    InMemoryRepository unit tests - findById

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,4 +1,5 @@
 import { Entity } from "../../../entity/entity";
+import { NotFoundError } from "../../../errors/not-found-error";
 import { InMemoryRepository } from "../../in-memory.repository";
 
 type StubEntityProps = {
@@ -23,5 +24,19 @@ describe("InMemoryRepository", () => {
         await sut.insert(entity);
 
         expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON());
+    });
+
+    it("Should throw error when entity not found",async ()=>{
+        await expect(sut.findById("dasdasda")).rejects.toThrow(new NotFoundError("Entity not found"));
+    });
+
+    it("Should find a entity by id",async ()=>{
+        const entity = new StubEntity({ name: "Algo", price: 100 });
+
+        await sut.insert(entity);
+
+        const result = await sut.findById(entity.id);
+
+        expect(entity.toJSON()).toStrictEqual(result.toJSON());
     });
 });
